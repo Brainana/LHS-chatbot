14c489276e8a553f41f072e7aed39d8f6dc678c1
Brainana <jerry.max.xu@gmail.com>
2025-05-25 14:07:59 -0400

    cleaned up setup.sh to remove unused variables

--- setup.sh
+++ setup.sh
@@ -11,9 +11,6 @@ port = $PORT\n\
 " >> /app/.streamlit/config.toml
 
 sed -i "s/your-api-key-here/$OPENAI_API_KEY/" /app/.streamlit/secrets.toml
-sed -i "s/your-claude-api-key-here/$CLAUDE_API_KEY/" /app/.streamlit/secrets.toml
-sed -i "s/your-trubrics-email-here/$TRUBRICS_EMAIL/" /app/.streamlit/secrets.toml
-sed -i "s/your-trubrics-pswd-here/$TRUBRICS_PASSWORD/" /app/.streamlit/secrets.toml
 
 sed -i "s/chroma-ip-here/$CHROMA_IP/" /app/config_remotedb.json
 sed -i "s/chroma-port-here/$CHROMA_PORT/" /app/config_remotedb.json
