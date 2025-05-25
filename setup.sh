02d1bdf6293509309e275d5e4f3f365e141fec42
Brainana <jerry.max.xu@gmail.com>
2025-05-25 14:24:54 -0400

    fixed issue with slashes for webserver url in setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ sed -i "s/your-api-key-here/$OPENAI_API_KEY/" /app/.streamlit/secrets.toml
 
 sed -i "s/chroma-ip-here/$CHROMA_IP/" /app/config_remotedb.json
 sed -i "s/chroma-port-here/$CHROMA_PORT/" /app/config_remotedb.json
-sed -i "s/doc-webserver-url-here/$DOC_WEBSERVER_URL/" /app/config_remotedb.json
+sed -i "s|doc-webserver-url-here|$DOC_WEBSERVER_URL|" /app/config_remotedb.json
 
 
 
